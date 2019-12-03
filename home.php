1c52a32a782a7fc7895e2bfbc50dab95137c0470
abarnasri <36784030+abarnasri@users.noreply.github.com>
2019-12-03 16:53:17 -0500

    Update home.php

--- home.php
+++ home.php
@@ -20,13 +20,13 @@ Order Doctors by:
 <br>
 <form action="ordereddocs.php" method="post">
 <br>
-<input name="radio" type="radio" value="First Name Increasing">First Name Increasing
+<input name="radio1" type="radio" value="First Name Increasing">First Name Increasing
 <br>
-<input name="radio" type="radio" value="First Name Decreasing">First Name Decreasing
+<input name="radio2" type="radio" value="First Name Decreasing">First Name Decreasing
 <br>
-<input name="radio" type="radio" value="First Name Ascending">Last Name Increasing
+<input name="radio3" type="radio" value="First Name Ascending">Last Name Increasing
 <br>
-<input name="radio" type="radio" value="First Name Descending">Last Name Decreasing
+<input name="radio4" type="radio" value="First Name Descending">Last Name Decreasing
 <br>
 <?php 
 include'ordereddocs.php'; 
