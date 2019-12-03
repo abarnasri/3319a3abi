76add52be8d57aed16ff113034e1154bb2ea2221
abarnasri <abarna_sri@hotmail.com>
2019-12-03 02:12:34 +0000

    Initial Commit

---
+++ tester.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta content="text/html; charset=ISO-8859-1"
+http-equiv="content-type">
+<title>Home</title>
+<style type="text/css">
+h1 {
+font-size: 20em;
+color: black;
+}
+
+</style>
+</head>
+<body>
+Abarna's Doctor Database<br>
+</body>
+</html>
+
