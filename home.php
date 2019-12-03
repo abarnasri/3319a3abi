22b511f559149bbd976619f5a8577a450973e1e9
abarnasri <36784030+abarnasri@users.noreply.github.com>
2019-12-03 14:58:10 -0500

    Update home.php

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Abarna's Doctor Database</title>
+	<link rel="stylesheet" type="text/css" href="asn3.css">
+</head>
+<body>
+<?php
+include 'connecttodb.php';
+?>
+<br>
+Order Doctors by: <br>
+<br>
+<br>
+<br>
+</body>
+</html>
