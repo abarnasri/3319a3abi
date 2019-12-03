d1db98121d9036654fbc39d020855c32702a0624
abarnasri <36784030+abarnasri@users.noreply.github.com>
2019-12-03 10:09:02 -0500

    Create ordereddocs.php

---
+++ ordereddocs.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Abarna's Doctor Database</title>
+	<link rel="stylesheet" type="text/css" href="asn3.css">
+</head>
+<body>
+<?php
+include 'connecttodb.php';
+?>
+<h1>Ordered Doctors:</h1>
+
+<?php
+
+if (isset($_POST['submit'])) { 
+	if(isset($_POST['radio'])) {
+		echo '<form action="displaydocinfo.php" method="post">';
+		$query = 'SELECT * FROM doctor ORDER BY firstName ASC';
+		$result = mysqli_query($connection,$query);
+		if (!$result) {
+			die("databases query failed.");
+		}
+		echo "<ol>";
+		while ($row = mysqli_fetch_assoc($result)) {
+			echo "<br>";
+			echo '<input type="radio" name="docs" value="';
+			echo $row["licenseNum"];
+			echo $row["firstName"] . " " . $row["lastName"] . "<br>";
+		}
+		mysqli_free_result($result);
+		echo "</ol>";
+		echo '<input type="submit" value="Display Doctor Info">';
+		echo '</form>';
+	}
+	
+
+	if(isset($_POST['radio'])) {
+		echo '<form action="displaydocinfo.php" method="post">';
+		$query = 'SELECT * FROM doctor ORDER BY firstName DESC';
+		$result = mysqli_query($connection,$query);
+		if (!$result) {
+			die("databases query failed.");
+		}
+		echo "<ol>";
+		while ($row = mysqli_fetch_assoc($result)) {
+			echo "<br>";
+			echo '<input type="radio" name="docs" value="';
+			echo $row["licenseNum"];
+			echo $row["firstName"] . " " . $row["lastName"] . "<br>";
+		}
+		mysqli_free_result($result);
+		echo "</ol>";
+		echo '<input type="submit" value="Display Doctor Info">';
+		echo '</form>';
+	}
+
+	if(isset($_POST['radio'])) {
+		echo '<form action="displaydocinfo.php" method="post">';
+		$query = 'SELECT * FROM doctor ORDER BY lastName ASC';
+		$result = mysqli_query($connection,$query);
+		if (!$result) {
+			die("databases query failed.");
+		}
+		echo "<ol>";
+		while ($row = mysqli_fetch_assoc($result)) {
+			echo "<br>";
+			echo '<input type="radio" name="docs" value="';
+			echo $row["licenseNum"];
+			echo $row["firstName"] . " " . $row["lastName"] . "<br>";
+		}
+		mysqli_free_result($result);
+		echo "</ol>";
+		echo '<input type="submit" value="Display Doctor Info">';
+		echo '</form>';
+	}
+
+	if(isset($_POST['radio'])) {
+		echo '<form action="displaydocinfo.php" method="post">';
+		$query = 'SELECT * FROM doctor ORDER BY lastName DESC';
+		$result = mysqli_query($connection,$query);
+		if (!$result) {
+			die("databases query failed.");
+		}
+		echo "<ol>";
+		while ($row = mysqli_fetch_assoc($result)) {
+			echo "<br>";
+			echo '<input type="radio" name="docs" value="';
+			echo $row["licenseNum"];
+			echo $row["firstName"] . " " . $row["lastName"] . "<br>";
+		}
+		mysqli_free_result($result);
+		echo "</ol>";
+		echo '<input type="submit" value="Display Doctor Info">';
+		echo '</form>';
+	}
+}
+?>
+</body>
+</html>
