ae2b94d62872653aab43071ae782435e7d3a3121
abarnasri <36784030+abarnasri@users.noreply.github.com>
2019-12-03 10:08:30 -0500

    Create displaydocinfo.php

---
+++ displaydocinfo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Abarna's Doctor Database</title>
+	<link rel="stylesheet" type="text/css" href="asn3.css">
+</head>
+<body>
+<?php
+include 'connecttodb.php';
+?>
+<h1>Doctor Information:</h1>
+<ol>
+<?php
+	$docLicenseNum= $_POST["docs"];
+	$query = 'SELECT * FROM doctor, hospital WHERE doctor.hospID=hospital.hospCode AND doctor.licenseNum="' . $docLicenseNum. '"';
+	$result=mysqli_query($connection,$query);
+	if (!$result) {
+		die("Database query failed.");
+	}
+	while ($row=mysqli_fetch_assoc($result)){
+		echo "<li>";
+		echo "<b>Doctor Name: </b>" . $row["firstName"] . " " . $row["lastName"] ." " . "<b>License No.: </b>" . " " . $row["licenseNum"] . " " . "<b>License Date: </b>" . " " . $row["licenseDate"] . " " . "<b>Specialty: </b>" . " " . $row["specialty"] . " " . "<b>Hospital Name: </b>" . " " . $row["hospName"] . "</li>";
+		echo '<img src="'.$row["docimage"].'" height="80" width="80">';
+	}
+	mysqli_free_result($result);
+?>
+</ol>
+<?php
+   mysqli_close($connection);
+?>
+</body>
+</html>
