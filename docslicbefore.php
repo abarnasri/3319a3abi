eaeddc27740b5cda3191c31e57348c9c7149a1c3
abarnasri <36784030+abarnasri@users.noreply.github.com>
2019-12-03 22:00:09 -0500

    Update docslicbefore.php

--- docslicbefore.php
+++ docslicbefore.php
@@ -12,13 +12,23 @@ include 'connecttodb.php';
 <ul>
 
 <?php
-
-<br>
-
-
-
+	//based on submit button from home
+	$date = $_POST["date"];
+	//get all doctors licensed before given date
+	$query = 'SELECT * FROM doctor WHERE licenseDate < "' . $date . '"';
+	$result = mysqli_query($connection,$query);
+	//if query fails 
+	if (!$result) {
+		die("Database query failed.");
+	}
+	//go through each row and print out the necessary information
+	while ($row=mysqli_fetch_assoc($result)){
+		echo "<li>";
+		echo "<b>Doctor Name:</b>" . $row["firstName"] . " " . $row["lastName"] . " " . "<b>Speciality: </b>" . " " . $row["speciality"] . " " . "<b>License Date: </b>" . " " . $row["licenseDate"] . "</li>"; 
+		echo '<img src="'.$row["docimage"].'" height="400" width="400">';
+	}
+	mysqli_free_result($result);
 ?>
-
 </ul>
 <?php
    mysqli_close($connection);
