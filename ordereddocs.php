4587bd525c257ebef83d93764895070dd2da531e
abarnasri <36784030+abarnasri@users.noreply.github.com>
2019-12-03 21:06:33 -0500

    Update ordereddocs.php

--- ordereddocs.php
+++ ordereddocs.php
@@ -12,16 +12,19 @@ include 'connecttodb.php';
 
 <?php
 
-	
+//check if submit button was selected
 if (isset($_POST['submit'])) { 
+	//if doctors need to be ordered in first name ascending
 	if(($_POST['radio'])=='First Name Increasing') {
 		$query = 'SELECT * FROM doctor ORDER BY firstName ASC';
 		$result = mysqli_query($connection,$query);
+		//if query fails
 		if (!$result) {
 			die("databases query failed.");
 		}
 		echo '<form action="displaydocinfo.php" method="POST">';
 		echo "<ol>";
+		//print doctor first name and last name
 		while ($row = mysqli_fetch_assoc($result)) {
 			echo "<br>";
 			echo '<input type="radio" name="doctors" value="';
@@ -33,14 +36,17 @@ if (isset($_POST['submit'])) {
 		echo '<input name="getinfo" type="submit" value="Display Doctor Info">';
 	}
 	
+	//if doctors need to be ordered in first name descending
 	if(($_POST['radio'])=='First Name Decreasing') {
 		$query = 'SELECT * FROM doctor ORDER BY firstName DESC';
 		$result = mysqli_query($connection,$query);
+		//if query fails
 		if (!$result) {
 			die("databases query failed.");
 		}
 		echo '<form action="displaydocinfo.php" method="POST">';
 		echo "<ol>";
+		//print doctor first name and last name
 		while ($row = mysqli_fetch_assoc($result)) {
 			echo "<br>";
 			echo '<input type="radio" name="doctors" value="';
@@ -51,15 +57,18 @@ if (isset($_POST['submit'])) {
 		echo "</ol>";
 		echo '<input name="getinfo" type="submit" value="Display Doctor Info">';
 	}
-
+	
+	//if doctors need to be ordered in last name ascending
 	if(($_POST['radio'])=='Last Name Increasing') {
 		$query = 'SELECT * FROM doctor ORDER BY lastName ASC';
 		$result = mysqli_query($connection,$query);
+		//if query fails
 		if (!$result) {
 			die("databases query failed.");
 		}
 		echo '<form action="displaydocinfo.php" method="POST">';
 		echo "<ol>";
+		//print doctor first name and last name
 		while ($row = mysqli_fetch_assoc($result)) {
 			echo "<br>";
 			echo '<input type="radio" name="doctors" value="';
@@ -71,14 +80,17 @@ if (isset($_POST['submit'])) {
 		echo '<input name="getinfo" type="submit" value="Display Doctor Info">';
 	}
 
+	//if doctors need to be ordered in last name descending
 	if(($_POST['radio'])=='Last Name Decreasing') {
 		$query = 'SELECT * FROM doctor ORDER BY lastName DESC';
 		$result = mysqli_query($connection,$query);
+		//if query fails
 		if (!$result) {
 			die("databases query failed.");
 		}
 		echo '<form action="displaydocinfo.php" method="POST">';
 		echo "<ol>";
+		//print doctor first name and last name
 		while ($row = mysqli_fetch_assoc($result)) {
 			echo "<br>";
 			echo '<input type="radio" name="doctors" value="';
