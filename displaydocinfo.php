6781c8a7bac0fb4eddd73e2ae0c31ec8c4ef0bcf
abarnasri <36784030+abarnasri@users.noreply.github.com>
2019-12-03 20:32:49 -0500

    Update displaydocinfo.php

--- displaydocinfo.php
+++ displaydocinfo.php
@@ -11,16 +11,19 @@ include 'connecttodb.php';
 <h1>Doctor Information:</h1>
 <ol>
 <?php
+	//based on previous submit button
 	$docLicenseNum = $_POST["doctors"];
+	//do query
 	$query = 'SELECT * FROM doctor, hospital WHERE doctor.hosWorksAt=hospital.hosCode AND doctor.docLicNum="' . $docLicenseNum. '"';
 	$result=mysqli_query($connection,$query);
 	if (!$result) {
 		die("Database query failed.");
 	}
+	//go through each row and print out the necessary information
 	while ($row=mysqli_fetch_assoc($result)){
 		echo "<li>";
 		echo "<b>Doctor Name: </b>" . $row["firstName"] . " " . $row["lastName"] . " " . "<b>License No.: </b>" . " " . $row["docLicNum"] . " " . "<b>License Date: </b>" . " " . $row["licenseDate"] . " " . "<b>Specialty: </b>" . " " . $row["speciality"] . " " . "<b>Hospital Name: </b>" . " " . $row["name"] . "</li>";
-		echo '<img src="'.$row["docimage"].'" height="80" width="80">';
+		echo '<img src="'.$row["docimage"].'" height="400" width="400">';
 	}
 	mysqli_free_result($result);
 ?>
