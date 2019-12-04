948ae75281e567395409b294169e57e3ff794123
abarnasri <36784030+abarnasri@users.noreply.github.com>
2019-12-03 22:23:41 -0500

    Update home.php

--- home.php
+++ home.php
@@ -27,7 +27,7 @@
 </form>
 
 <br>
-	<b>Doctors Licensed Before A Certain Date:</b>
+	<b>Doctors Licensed Before A Certain Date</b>
 <form action="docslicbefore.php" method="post">
 Enter Date (YYYY-MM-DD): <input type="text" name="date">
 <br>
@@ -35,6 +35,29 @@ Enter Date (YYYY-MM-DD): <input type="text" name="date">
 <input type="submit" value="Get Doctors Before This Date">
 </form>
 	
+<br>
+	<b>Enter New Doctor </b>
+<form action="newdoc.php" method="post" enctype="multipart/form-data">
+First Name: <input type="text" name="first">
+<br>
+Last Name: <input type="text" name="last">
+<br>
+Speciality: <input type="text" name="speciality">
+<br>	
+License Date (YYYY-MM-DD): <input type="text" name="licenDate">
+<br>	
+License No.(e.g. AA111): <input type="text" name="LicenNum">
+<br>
+Hospital Name: 
+<input name="hosp" type="radio" value='ABC'>Victoria, London ON
+<input name="hosp" type="radio" value='DDE'>Victoria, Victoria BC
+<input name="hosp" type="radio" value='BBC'>St.Joseph, London ON
+<br>
+Add Image of Doctor: 
+<form>
+<input type="file" name="file" id="file"><br>
+<input type="submit" value="Add Doctor">
+</form>
 	
 	
 
